0c63047a450a284d0271622d656e37c0befffed3
Joerg <joerg.doepfert@flixbus.com>
2017-05-07 20:16:36 +0200

    implement selectors

--- my_model_selectors.py
+++ my_model_selectors.py
@@ -66,18 +66,45 @@ class SelectorBIC(ModelSelector):
 
     http://www2.imm.dtu.dk/courses/02433/doc/ch6_slides.pdf
     Bayesian information criteria: BIC = -2 * logL + p * logN
+
+    According to https://en.wikipedia.org/wiki/Hidden_Markov_model, there are
+    n*(n-1) transition parameters, where n is the number of states.
+    Furthermore, for reach of the n states, 2*m parameters controlling the
+    multivariate Gaussian (assuming a diagonal covariance matrix as used here),
+    where m is the number of dimensions (i.e. the number of features).
+    Therefore, the number of parameters p can be computed as follows:
+    p = n*(n-1) + n*(2*m)
     """
 
     def select(self):
         """ select the best model for self.this_word based on
         BIC score for n between self.min_n_components and self.max_n_components
 
+
         :return: GaussianHMM object
         """
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on BIC scores
-        raise NotImplementedError
+        possible_n_components = list(range(self.min_n_components,
+                                           self.max_n_components+1))
+        scores = []
+        for n_components in possible_n_components:
+            model = self.base_model(n_components)
+            try:
+                logL = model.score(self.X, self.lengths)
+            except Exception as e:
+                print("n = {}: Scoring did not work due to exception '{}'".format(n_components, e))
+                print("Set logL to -Inf")
+                logL = -float('Inf')
+            n = model.n_components  # equals `n_components`
+            m = model.n_features
+            p = n*(n-1) + n*(2*m)  # see class docstring
+            N = len(self.X)  # each feature vector in `X` is a data point
+            logN = np.log(N)
+            BIC = -2 * logL + p * logN
+            scores.append(BIC)
+        best_num_components = possible_n_components[scores.index(min(scores))]
+        return self.base_model(best_num_components)
 
 
 class SelectorDIC(ModelSelector):
@@ -87,13 +114,43 @@ class SelectorDIC(ModelSelector):
     Document Analysis and Recognition, 2003. Proceedings. Seventh International Conference on. IEEE, 2003.
     http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.58.6208&rep=rep1&type=pdf
     DIC = log(P(X(i)) - 1/(M-1)SUM(log(P(X(all but i))
+    M: number of classes (i.e. words)
+    log(P(X(i)): log likelihood of word i
     '''
 
     def select(self):
         warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-        # TODO implement model selection based on DIC scores
-        raise NotImplementedError
+        possible_n_components = list(range(self.min_n_components,
+                                           self.max_n_components+1))
+        M = len(self.hwords.keys())  # number of classes/words
+        scores = []
+        for n_components in possible_n_components:
+            model = self.base_model(n_components)
+            try:
+                logL = model.score(self.X, self.lengths)
+            except Exception as e:
+                print("n = {}: Scoring did not work due to exception '{}'".format(n_components, e))
+                print("Set DIC to -Inf")
+                scores.append(float('-Inf'))
+                break
+
+            other_words = [w for w in self.hwords.keys() if self.this_word != w]
+            sum_anti_likelihoods = 0
+            for word in other_words:
+                X, lengths = self.hwords[word]
+                try:
+                   sum_anti_likelihoods += model.score(X, lengths)
+                except Exception as e:
+                    print("n = {}: Scoring did not work due to exception '{}'".format(n_components, e))
+                    print("Set DIC to -Inf")
+                    scores.append(float('-Inf'))
+                    break
+            DIC = logL - 1/(M - 1) * sum_anti_likelihoods
+            scores.append(DIC)
+
+        best_num_components = possible_n_components[scores.index(max(scores))]
+        return self.base_model(best_num_components)
 
 
 class SelectorCV(ModelSelector):
@@ -127,4 +184,8 @@ class SelectorCV(ModelSelector):
             scores.append(cv_score)
 
         best_num_components = possible_n_components[scores.index(max(scores))]
+
+        # reset self.X and self.lenghts to original value
+        self.X, self.lengths = self.hwords[self.this_word]
+        # return fitted model with best parameters
         return self.base_model(best_num_components)
